63b00e195ffca5ac418aecf5b17340f263fb5246
pablomartan <pablomartan@kolabnow.com>
2023-03-28 12:12:19 +0200

    feat: add basic DrumPad component

---
+++ src/js/react/DrumPad.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export class DrumPad extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return(
+      <>
+        <div className='drum-pad' />
+      </>
+    )
+  }
+}
