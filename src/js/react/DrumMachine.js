f8c1b0b8de132920b50e5e51871d00bf774d1a87
pablomartan <pablomartan@kolabnow.com>
2023-03-28 12:12:02 +0200

    feat: add basic DrumMachine component

---
+++ src/js/react/DrumMachine.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export class DrumMachine extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return(
+      <>
+        <div id="drum-machine" />
+      </>
+    )
+  }
+}
