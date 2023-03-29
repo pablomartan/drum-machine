4cb436c3a4f7c5bbc57641138fb461e1e3c6f974
pablomartan <pablomartan@kolabnow.com>
2023-03-29 19:00:09 +0200

    feat: DrumPad component finished, with onclick function included
    The drumpad should play the sound when clicked, and the onclick function
    takes care of this by selecting the audio element and calling is play()
    function

--- src/js/react/DrumPad.js
+++ src/js/react/DrumPad.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Audio } from './Audio.js';
 
 export class DrumPad extends React.Component {
   constructor(props) {
@@ -6,10 +7,14 @@ export class DrumPad extends React.Component {
   }
 
   render() {
+    const audId = this.props.id;
+    const audSrc = this.props.src;
+
     return(
-      <>
-        <div className='drum-pad' />
-      </>
+        <div className='drum-pad' onClick={() => { document.getElementById(audId).play() }}>
+          <p>{audId}</p>
+          <Audio id={audId} src={audSrc} />
+        </div>
     )
   }
 }
