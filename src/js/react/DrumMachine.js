fcfc78d4d027189f2c8e4d3bed710707954af09b
pablomartan <pablomartan@kolabnow.com>
2023-03-29 19:30:31 +0200

    feat: add event listener to play sounds on keystroke
    Added an event listener to body for keypresses and play the sound when
    the key corresponds to a drumpad

--- src/js/react/DrumMachine.js
+++ src/js/react/DrumMachine.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { DrumPadContainer } from './DrumPadContainer.js';
+import { Display } from './Display.js';
 
 export class DrumMachine extends React.Component {
   constructor(props) {
@@ -6,10 +8,20 @@ export class DrumMachine extends React.Component {
   }
 
   render() {
+    document.body.addEventListener('keypress', e => {
+      const k = e.key.toUpperCase();
+      try {
+        document.getElementById(k).play();
+      } catch (e) {
+        console.log(e);
+      }
+    });
+
     return(
-      <>
-        <div id="drum-machine" />
-      </>
+      <div id="drum-machine">
+        <DrumPadContainer />
+        <Display />
+      </div>
     )
   }
 }
