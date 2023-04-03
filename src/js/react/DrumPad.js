8270e2418fcb399afa5cf05a2b3cfcea48077a6e
pablomartan <pablomartan@kolabnow.com>
2023-04-03 18:48:24 +0200

    changes to DrumPad component; see full msg
    * Added function to play sample on click
    * Renamed Audio id property
    * Added Bootstrap

--- src/js/react/DrumPad.js
+++ src/js/react/DrumPad.js
@@ -1,18 +1,34 @@
 import React from 'react';
 import { Audio } from './Audio.js';
 
+/**
+ * @description: a drumpad. It can be played by clicking it or pressing the key
+ * it displays
+ */
 export class DrumPad extends React.Component {
   constructor(props) {
     super(props)
   }
 
-  render() {
-    const audId = this.props.id;
-    const audSrc = this.props.src;
+  handleClick = e => {
+    e.preventDefault();
+    let targ = e.target;
+    if (targ.nodeName != 'DIV') {
+      targ = targ.parentElement;
+    }
+    document.getElementById(this.props.audId).play();
+    document.getElementById('display').innerText = this.props.id;
+    targ.classList.toggle('play');
+    setTimeout(() => targ.classList.toggle('play'), 500);
+  }
 
+  render() {
+    const audId = this.props.audId;
+    const audSrc = this.props.src;
+    
     return(
-        <div className='drum-pad' onClick={() => { document.getElementById(audId).play() }}>
-          <p>{audId}</p>
+        <div className='drum-pad d-flex justify-content-center align-items-center' id={this.props.id} onClick={this.handleClick}>
+          <p className='m-auto'>{audId}</p>
           <Audio id={audId} src={audSrc} />
         </div>
     )
