43b8fb0132e0afee5d6564704bfa44c9689db9f0
pablomartan <pablomartan@kolabnow.com>
2023-04-03 18:51:10 +0200

    feat: add entry point

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { DrumMachine } from './js/react/DrumMachine.js';
+
+import style from './style/main.scss';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<DrumMachine />);
