3a9e30442566f0e6c175728be803b7dabbe5c5dd
pablomartan <pablomartan@kolabnow.com>
2023-04-03 18:45:59 +0200

    changes to DrumPadContainer component; see full msg
    * Mapped key names to sample names to build drumpads programatically and
      predictably
    * Added event listener to play sample on keypress to body
    * Added id for DrumPad and renamed id for Audio component inside DrumPad

--- src/js/react/DrumPadContainer.js
+++ src/js/react/DrumPadContainer.js
@@ -5,7 +5,7 @@ export class DrumPadContainer extends React.Component {
   constructor(props) {
     super(props)
   }
-
+  
   render() {
     const links = [
       'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3',
@@ -19,12 +19,39 @@ export class DrumPadContainer extends React.Component {
       'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'
     ];
 
-    const letters = [ 'Q', 'W', 'E', 'A', 'S', 'D', 'Z', 'X', 'C' ];
+    const letterSampleMap = {
+      'Q': 'Heater 1',
+      'W': 'Heater 2',
+      'E':'Heater 3',
+      'A': 'Heater 4',
+      'S': 'Clap',
+      'D': 'Open-HH',
+      'Z': 'Kick-n\'-hat',
+      'X': 'Kick',
+      'C': 'Closed-HH',
+    }
+
+    const letters = Object.keys(letterSampleMap);
+    
+    document.body.addEventListener('keypress', e => {
+      const k = e.key.toUpperCase();
+      if (letters.includes(k)) {
+        const audio = document.getElementById(k);
+        const pad = audio.parentElement;
+        audio.play();
+        document.getElementById('display').innerText = letterSampleMap[k];
+        // set pad's class to play to appear to be pressed;
+        pad.classList.toggle('play');
+        setTimeout(() => pad.classList.toggle('play'), 500);
+      }
+    });
+
 
     return(
     <div id='pad-container'>
       {letters.map((l, i) => {
-        return(<DrumPad key={i} id={l} src={links[i]} />);
+        return(<DrumPad key={i} id={letterSampleMap[l]}
+              audId={l} src={links[i]} />);
       })}
     </div>
     )
